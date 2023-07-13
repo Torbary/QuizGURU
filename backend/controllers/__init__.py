9fb3cc67c413c3247ac891c83a2d8dc8145f980d
Akanni Emmanuel <102362366+coderoyalty@users.noreply.github.com>
2023-07-13 12:24:09 -0700

    controller system (#6)
    * created a singleton class.
    
    * created a question controller module
    
    * create a quiz controller module
    - get_quiz: fetch a quiz data
    - fetch_quizzes: retrieves quizzes based on pagination
    
    * new: new controller function for quiz and question
    - fetch_questions: method to fetch all questions of a quiz.
    - post_quiz: method to create a quiz.
    - post_question: method to handle quiz question creation.
    - post_questions: method to create many questions for a quiz.

--- backend/controllers/__init__.py
+++ backend/controllers/__init__.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+class Singleton:
+    _instance = None
+    def __new__(self, *args, **kwargs):
+        if not self._instance:
+            self._instance = super().__new__(self)
+        return self._instance
